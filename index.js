06ed00e0b1a86373c4c13363aab81e8d94936b4f
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 08:11:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign(obj,{key:value})
 
 }
+function updateObjectWithKeyAndValue(obj, key, value){
+  
+  return Object.assign(obj,{key:value})
+
+}
