7cd9292f9641a1732dcda95a707ca38b374dae12
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 09:29:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value){
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
-  return newObj;
+  return newObj;}
 
 
 
