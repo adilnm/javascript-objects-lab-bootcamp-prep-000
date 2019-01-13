c3ae53c3058034a1c5cdc91f4f864aa1e7c33113
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 08:56:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ function deleteFromObjectByKey(obj, key, value){
   return Object.assign(obj,{key:value})
 
 }
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, valu){
-  obj[key]=value
-  return obj
-}
+
 
 updateObjectWithKeyAndValue(recipes, "kok", "1 lb")
 console.log(recipes)
